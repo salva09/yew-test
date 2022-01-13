8d1fb7a9da2570c3300794f251ab2948217a8d9b
salva <salva.hg01@gmail.com>
2022-01-12 22:55:38 -0600

    feat(screens): Add counter

---
+++ src/screens/counter.rs
@@ -0,0 +1,13 @@
+use yew::prelude::*;
+
+use crate::components::button_counter::ButtonCounter;
+
+#[function_component(Counter)]
+pub fn counter() -> Html {
+    html! {
+        <div>
+        <h1>{"Click the button to start counting!"}</h1>
+            <ButtonCounter />
+        </div>
+    }
+}
